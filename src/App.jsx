8de48ed3b73582f7442f7d3aec863dc386483db5
mh akash <mhakash5000@gmail.com>
2023-03-29 14:16:43 +0600

    added basic setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './components/Home/Home'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Home/>
+    </div>
+  )
+}
+
+export default App
